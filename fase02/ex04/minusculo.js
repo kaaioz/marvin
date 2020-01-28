a6f7c5e0c325f52082704f0e8d6678fc9620c6d2
kaaioz <59872554+kaaioz@users.noreply.github.com>
2020-01-28 09:47:31 -0300

    marvin4

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,10 +1,6 @@
 function minusculo(palavra) {
-    const palavras = palavra
-    return palavra.substring(0,1).concat(palavra.substring(1).toUpperCase())
+const palavras = palavra;
+return palavra.substring(1,0).toLowerCase()
+
 }
-
-
-
-
-
 
