be432828cc4f316136aebb549500e036a4d6266d
kaaioz <59872554+kaaioz@users.noreply.github.com>
2020-01-30 09:05:34 -0300

    kaio.marvin

---
+++ fase03/ex00/parOuImpar.js
@@ -0,0 +1,10 @@
+function parOuImpar(numero){
+    var numero = (numero)
+    var total = numero % 2;
+    if (total == 0) {
+        return ("Par")
+    }else {
+        return ("impar")
+    }
+}
+
