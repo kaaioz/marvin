d1ea5bda74ffc8cbba2cd9953a4d807f3f25e2e5
kaaioz <59872554+kaaioz@users.noreply.github.com>
2020-02-11 11:22:35 -0300

    kaio.marvin2

--- fase04/ex02/fizzbuzz.js
+++ fase04/ex02/fizzbuzz.js
@@ -1,19 +1,16 @@
 function fizzbuzz(num1, num2){
-    var a = num1
-    while (a<= num2){
-        if (a % 3 === 0 && a % 5 === 0){
-            console.log (a + " FizzBuzz ")
-            a++
+    var c = num1
+    while (c <= num2){
+        if (c % 3 === 0 && c % 5 === 0){
+            console.log (c + " FizzBuzz ")
+        } else if (c % 5 === 0){
+        console.log(c + " Buzz ")
+        } else if (c % 3 === 0){
+        console.log(c + " Fizz ")
+        } else {
+        console.log(c)
         }
-    if (a % 5 === 0){
-        console.log(a + " Buzz ")
-        a++
+      c++
     }
-    if (a % 3 === 0){
-        console.log(a + " Fizz ")
-        a++
 }
-console.log(a)
-a++
-}
-}
+    
